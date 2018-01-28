cbb821e1fbd1ca756587985bb5a27692aee485af
Harryson C. Guimarães <harryson.guimaraes@gmail.com>
2018-01-28 11:02:45 -0200

    Voltando sdk para o modo normal. Estava em uso o sdk com debug habilitado.

--- frontend/app/scripts/services/facebookService.js
+++ frontend/app/scripts/services/facebookService.js
@@ -8,7 +8,7 @@ var FacebookService = function() {
 			var js, fjs = d.getElementsByTagName(s)[0];
 			if (d.getElementById(id)) {return;}
 			js = d.createElement(s); js.id = id;
-			js.src = 'https://connect.facebook.net/en_US/sdk/debug.js';
+			js.src = 'https://connect.facebook.net/en_US/sdk/sdk.js';
 			fjs.parentNode.insertBefore(js, fjs);
 		}(document, 'script', 'facebook-jssdk'));
 
